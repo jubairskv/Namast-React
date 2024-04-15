3ec341b092184ffd8d37def2ebfa3867bbd6ab96
jubairskv <jubairskv@gmail.com>
2024-04-15 11:53:17 +0530

    added:new code

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
-const heading1=React.createElement("h1",{id:"title"},"Heading1")
-const heading2=React.createElement("h2",{id:"title"},"Heading2")
-const container=React.createElement("div",{id:"container"},[heading1,heading2])
-const root=ReactDOM.createRoot(document.getElementById("root"))
-//passing the react element inside the root
-root.render(container);
-console.log(container)
+// const heading1=React.createElement("h1",{id:"title"},"Heading1")
+// const heading2=React.createElement("h2",{id:"title"},"Heading2")
+// const container=React.createElement("div",{id:"container"},[heading1,heading2])
+// const root=ReactDOM.createRoot(document.getElementById("root"))
+// //passing the react element inside the root
+// root.render(container);
+// console.log(container)
+const heading=React.createElement("h1",{},"hi everyone");
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # Namast-React
-
+what is react?
 what is Emmet?
 what is CDN?
+CDN is a place where the react library is hosted and fetching the react putting into our code uisng CDN links [content delivery network lins]
+
+A content delivery network (CDN) is a netowrk of interconnected servers that speeds up webpage loading for data heavy applications
+
+why do we use CDN links?
+
+
 what is crossorigin?
 what is async and differ
 what is the shortest program of javascript? 
