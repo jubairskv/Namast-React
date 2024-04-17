eccabd3e6156a3a943d268f1417cc15f3f6399d3
jubairskv <jubairskv@gmail.com>
2024-04-17 16:11:35 +0530

    new

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom"
 // const heading1=React.createElement("h1",{id:"title"},"Heading1")
 // const heading2=React.createElement("h2",{id:"title"},"Heading2")
 // const container=React.createElement("div",{id:"container"},[heading1,heading2])
