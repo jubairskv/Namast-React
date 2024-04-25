c7df36881fc9e6f4519d82b692fcbd45e33a7e50
jubairskv <jubairskv@gmail.com>
2024-04-25 21:49:13 +0530

    store redux

--- src/utils/store.js
+++ src/utils/store.js
@@ -9,3 +9,29 @@ const store = configureStore({
 
 export default store
 
+/**
+ * create store
+ * configurstore()-redux toolkit (RTK)
+ * 
+ * provider my store in APP.js
+ * <Provider store={store}  -import from react redux
+ * 
+ * slice
+ * RTK-createslicer({
+ * name:""
+ * initialstate:
+ * reducers:
+ *        addItem(state,action)=>{
+ *         state=action.payload
+ * }
+ * export const{addItem,removeItem,clearItem}=cartslice.aaction
+ * export defuault cartSlice.reducers
+ * })
+ * 
+ * 
+ * put the slice into the store
+ * redcer:{
+ *      cart:cartSlice,
+ *       user:userSlice 
+ */
+
