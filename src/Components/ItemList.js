3f0431946c2caeee873397a9c0f4315591dbfdf4
jubairskv <jubairskv@gmail.com>
2024-05-15 15:01:43 +0530

    accordian done

--- src/Components/ItemList.js
+++ src/Components/ItemList.js
@@ -1,20 +1,33 @@
 import { CDN_URL } from "../utils/constant";
 
-const ItemList=({items})=>{
-    console.log(items)
-    return(
+const ItemList = (props) => {
+    const { items } = props
+    //const {id,name,price,description} = items?.card?.info
+    // console.log(id,name,price,des)
+    console.log(props)
+    return (
         <div>
-          {
-            items.map((items)=>(
-                <div key={items.card.info.id} className="p-2 m-2 border-gray-200 border-b-2 text-left">
-                    <img src={CDN_URL + items.card.info.imageid} className="w-14"/>
-                    <div className="py-2">
-                        <span>{items.card.info.name}</span>
-                        <span> - ₹{items.card.info.price ? items.card.info.price/100:items.card.info.defaultPrice/100}</span>
+            {
+                items.map((items) => (
+                    <div key={items.card.info.id} className="p-2 m-2 border-gray-200 border-b-2 text-left flex justify-between">
+                       
+                        <div className="w-9/12">
+                            <div className="py-2">
+                                <span>{items.card.info.name}</span>
+                                <span> - ₹{items.card.info.price ? items.card.info.price / 100 : items.card.info.defaultPrice / 100}</span>
+                            </div>
+                            <p>{items.card.info.description}</p>
+                        </div>
+                        <div className=" w-3/12 p-4">
+                            
+                            <div className="absolute ">
+                            <button className="p-2 mx-10 bg-lime-400 shadow-lg m-auto rounded-lg">Add +</button>
+                            </div>
+                            <img src={CDN_URL + items.card.info.imageId} className="w-full" />
+                            
+                            </div>
                     </div>
-                    <p>{items.card.info.description}</p>
-                </div>
-            ))}
+                ))}
         </div>
     );
 
--- src/Components/RestaurantCategory.js
+++ src/Components/RestaurantCategory.js
@@ -1,15 +1,23 @@
 import ItemList from "./ItemList";
+import { useState } from "react";
+
 
 const RestaurantCategory = ({ data }) => {
+    const [showItem ,setShowItem] = useState(false)
+
+     const handleClick = ()=>{
+        setShowItem(!showItem)
+     }
     //console.log(data)
+
     return (
         <div>
             <div className="w-6/12 mx-auto my-4 bg-gray-50 shadow-lg p-4">
-                <div className="flex justify-between">
+                <div className="flex justify-between cursor-pointer" onClick={handleClick}>
                     <span className="font-bold text-lg">{data.title}({data.itemCards.length})</span>
                     <span>⬇️</span>
                 </div>
-                <ItemList items={data.itemCards} />
+                {showItem && <ItemList items={data.itemCards} />}
             </div>
         </div>
     );
