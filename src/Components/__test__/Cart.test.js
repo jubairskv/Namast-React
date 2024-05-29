379f0a5099e6612e970c116cd32518d63f5d66b8
jubairskv <jubairskv@gmail.com>
2024-05-29 12:33:50 +0530

    test cases

--- src/Components/__test__/Cart.test.js
+++ src/Components/__test__/Cart.test.js
@@ -1,10 +1,12 @@
 import { fireEvent, render } from "@testing-library/react"
 import { act } from "@testing-library/react"
+import Header from "../Header"
 import ResturantMenu from "../RestaurantMenu"
 import Mock_datas from "../mocks/mockResMenu.json"
-import { render,screen } from "@testing-library/react"
+import {render,screen} from "@testing-library/react"
 import { Provider } from "react-redux"
 import store from "../../utils/store"
+import { BrowserRouter } from "react-router-dom"
 
 global.fetch=jest.fn(()=>{
     return Promise.resolve({
@@ -17,9 +19,12 @@ global.fetch=jest.fn(()=>{
 it("should load resturant menu component" ,async ()=>{
  await act (async ()=>{
     render(
+        <BrowserRouter>
         <Provider store={store}>
+            <Header/>
         <ResturantMenu/>
         </Provider>
+        </BrowserRouter>
     )
 
     const accordianHeader = screen.getByText("Dosa(8)")
@@ -28,5 +33,11 @@ it("should load resturant menu component" ,async ()=>{
     const foodItems = screen.getAllByTestId("food-Items")
 
     expect(foodItems.length).toBe(8)
+
+    const addbBtn = screen.getAllByRole("button",{name:"Add +"})
+    fireEvent.click(addbBtn[0])
+
+    const cart = screen.getByText("cart(1items")
+    expect(cart).toBeInTheDocument()
  })
 })
