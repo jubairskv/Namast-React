aa46b0f07ace3f1e072121d7c594da3daf9f80a6
jubairskv <jubairskv@gmail.com>
2024-08-13 17:52:39 +0530

    swigg

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -23,31 +23,31 @@ const Header = () => {
 
 
   return (
-    <div className="flex justify-between bg-white shadow-md sm:bg-white lg:bg-white">
+    <div className="flex justify-between  w-full h-[95px] bg-white shadow-md sm:bg-white lg:bg-white pr-64 " >
       <div className="logo-conatiner">
-        <img className="w-44" src={LOGO_URL} />
+        <img className="w-[100px]" src={LOGO_URL} />
       </div>
       <div className="flex items-center">
         <ul className="flex p-4 m-4">
-          <li className="px-4">
+          <li className="px-4 font-GilroyBold text-lg  text-color-gray ">
             <Link to="/Contact">OnlineStatus:{OnlineStatus ? "✅" : "🔴"}</Link>
           </li>
-          <li className="px-4 font-GilroyBold">
+          <li className="px-4 font-GilroyBold text-lg  text-color-gray ">
             {/* <a href="/">Home</a> */}
             <Link to="/">Home</Link>
           </li>
-          <li className="px-4">
+          <li className="px-4 font-GilroyBold text-lg  text-color-gray ">
             {/* <a href="/About">About us</a> */}
             <Link to="/About">Search</Link>
           </li>
-          <li className="px-4">
+          <li className="px-4 font-GilroyBold text-lg text-color-gray ">
             <Link to="/Contact">Offers</Link>
           </li>
-          <li className="px-4">
+          <li className="px-4 font-GilroyBold text-lg  text-color-gray ">
             <Link to="/Grocery">Help</Link>
           </li>
-          <li className="px-4 font-bold text-xl "> <Link to="/cart">Cart({cartItems.length}items)</Link> </li>
-          <button className="login-btn"
+          <li className="px-4 font-GilroyBold text-lg  text-color-gray "> <Link to="/cart">Cart{/*({cartItems.length}items)*/}</Link> </li>
+          <button className="login-btn  font-GilroyBold text-lg  text-color-gray  "
             onClick={() => {
               btnName === "Login" ?
               setbtnName("Logout") :
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,9 +13,9 @@ module.exports = {
         GilroySemiBold: ['Gilroy-semiBold', 'sans-serif'],
         GilroyMediumBold: ['Gilroy-mediumBold', 'sans-serif'],
       },
-      color:{
-        "bg-color-gray":"#3d4152",
-        "bg-color-graylight":"#9799a3"
+      colors:{
+        "color-gray":"#484c5c",
+        "color-graylight":"#9799a3",
       }
     },
   },
