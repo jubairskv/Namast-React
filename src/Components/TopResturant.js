d8f67b403e16be5e4190efff181f26484cd73219
jubairskv <jubairskv@gmail.com>
2024-08-15 16:45:47 +0530

    swigg

--- src/Components/TopResturant.js
+++ src/Components/TopResturant.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { CDN_URL } from "../utils/constant";
-import StartIcon from "../Assets/"
+
+
 
 const TopResturant = ({ resData }) => {
   console.log(resData);
@@ -8,7 +9,7 @@ const TopResturant = ({ resData }) => {
     <div>
       <img src={CDN_URL + resData[0].info.cloudinaryImageId} alt="TopResturant"  className="w-64 h-48 rounded-2xl"/>
       <h1 className="font-GilroyBold pt-3 pl-3">{resData[0].info.name}</h1>
-      <img src={CDN_URL + "Rxawards/_CATEGORY-Burger.png"}/> 
+      
     </div>
   );
 };
