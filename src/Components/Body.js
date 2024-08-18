11c458a2b546e69357fe0ad0841a9585d5dfc0ec
jubairskv <jubairskv@gmail.com>
2024-08-18 16:20:26 +0530

    swigg

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -161,7 +161,7 @@ const Body = () => {
   const fetchData = async () => {
     try {
       const data = await fetch(
-        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING&nextOffset=${nextOffset}`
       );
       const json = await data.json();
       setRes(
